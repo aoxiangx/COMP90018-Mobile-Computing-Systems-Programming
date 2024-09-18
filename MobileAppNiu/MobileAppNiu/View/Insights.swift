7708e1c3e48b44c5a68c968e569d25cfcd4606c2
jun9a <jiuyuz@student.unimelb.edu.au>
2024-09-18 12:40:53 +1000

    Updated Front End View

---
+++ MobileAppNiu/MobileAppNiu/View/Insights.swift
@@ -0,0 +1,18 @@
+//
+//  Insights.swift
+//  MobileAppNiu
+//
+//  Created by Jun Zhu on 17/9/2024.
+//
+
+import SwiftUI
+
+struct Insights: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Insights()
+}
---
+++ MobileAppNiu/MobileAppNiu/View/SmallGraph.swift
@@ -0,0 +1,18 @@
+//
+//  SmallGraph.swift
+//  MobileAppNiu
+//
+//  Created by Jun Zhu on 16/9/2024.
+//
+
+import SwiftUI
+
+struct SmallGraph: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SmallGraph()
+}
