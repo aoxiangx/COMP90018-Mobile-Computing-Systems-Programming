478f88b7d0b8becfc0c7e1cf07ec60084e7e9a87
AHao23333 <hgguan@student.unimelb.edu.au>
2024-09-21 00:22:16 +1000

    feat: logout view and new home view

--- MobileAppNiu/MobileAppNiu/View/LogoutView.swift
+++ MobileAppNiu/MobileAppNiu/View/LogoutView.swift
@@ -1,19 +1,22 @@
 //
-//  HomeView.swift
+//  LogoutView.swift
 //  MobileAppNiu
 //
-//  Created by 关昊 on 14/9/2024.
+//  Created by 关昊 on 21/9/2024.
 //
 
 import SwiftUI
 import Firebase
 import FirebaseAuth
 
-struct HomeView: View {
-    @EnvironmentObject var manager : HealthManager
+struct LogoutView: View {
+    
     @AppStorage("log_Status") private var logStatus: Bool = false
     
+    
     var body: some View {
+        
+        
         ZStack {
             // 背景
             LinearGradient(gradient: Gradient(stops: [
@@ -26,18 +29,18 @@ struct HomeView: View {
             
             // 页面内容
             
-            Button("Home") {
-            }
-            .onAppear {
-                manager.fetchTodaySteps()
+            Button("logOut") {
+                try? Auth.auth().signOut()
+                logStatus = false
             }
             .navigationTitle("Home")
+            .padding()
             
             
         }
     }
 }
+
 #Preview {
-    HomeView()
-        .environmentObject(HealthManager())
+    LogoutView()
 }
