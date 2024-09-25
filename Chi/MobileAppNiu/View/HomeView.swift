d8be208558ec393cdd78ac68967dfcd8e8aa53a2
AHao23333 <hgguan@student.unimelb.edu.au>
2024-09-26 00:15:15 +1000

    feat: photo button view allow user upload photo

--- Chi/MobileAppNiu/View/HomeView.swift
+++ Chi/MobileAppNiu/View/HomeView.swift
@@ -66,7 +66,7 @@ struct HomeView: View {
                 .padding(.leading, 15)
                 
                 
-                
+                PhotoButtonView()
             }
         }
     }
