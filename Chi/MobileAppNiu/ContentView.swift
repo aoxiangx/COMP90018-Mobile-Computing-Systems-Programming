110dbfca3878200d4abc4b6941fe9941249f7ac1
ToriLi1210 <hanying1@student.unimelb.edu.au>
2024-10-04 01:46:19 +1000

    feat(contentview): add tab navigation bar

--- Chi/MobileAppNiu/ContentView.swift
+++ Chi/MobileAppNiu/ContentView.swift
@@ -11,18 +11,69 @@ import AuthenticationServices
 struct ContentView: View {
     
     @AppStorage("log_Status") private var logStatus: Bool = true // remember to set as false
-    
+    @State private var isReflectToggled = false
+    @State private var isJourneyToggled = false
+    @State private var isInsightsToggled = false
+    @State private var isSettingsToggled = false
     var body: some View {
+        if logStatus{
+            TabView {
+                HomeView()
+                    .tabItem {
+                        Image(isReflectToggled ? .mindfulnessToggled : .mindfulUntoggled) // Toggle between two images
+                        Text("Reflect")
+                    }
+                    .onAppear {
+                        toggleTabs(reflect: true) // Toggle Reflect and untoggle others
+                    }
+                
+                CalendarView()
+                    .tabItem {
+                        Image(isJourneyToggled ? .journeyToggled : .journeyUntoggled)
+                        Text("Journey")
+                    }
+                    .onAppear {
+                        toggleTabs(journey: true) // Toggle Journey and untoggle others
+                    }
+                
+                SummaryBoxesView().environmentObject(HealthManager())
+                    .tabItem {
+                        Image(isInsightsToggled ? .insightsToggled : .insightsUntoggled)
+                        Text("Insights")
+                    }
+                    .onAppear {
+                        toggleTabs(insights: true) // Toggle Insights and untoggle others
+                    }
+                
+                ProfileView()
+                    .tabItem {
+                        Image(isSettingsToggled ? .settingToggled : .settingUntoggled)
+                        Text("Setting")
+                    }
+                    .onAppear {
+                        toggleTabs(settings: true) // Toggle Settings and untoggle others
+                    }
+            }
+     
+        } else{
+            LoginView()
+        }
 
 //        BackgroundView()
             //            HomeView()
             //            GroupingDataView()
-        if logStatus{
-            SummaryBoxesView().environmentObject(HealthManager())
-        } else{
-            LoginView()
-        }
+//        if logStatus{
+//            SummaryBoxesView().environmentObject(HealthManager())
+//        } else{
+//            LoginView()
+//        }
         
+    }
+    private func toggleTabs(reflect: Bool = false, journey: Bool = false, insights: Bool = false, settings: Bool = false) {
+            isReflectToggled = reflect
+            isJourneyToggled = journey
+            isInsightsToggled = insights
+            isSettingsToggled = settings
     }
 }
 
