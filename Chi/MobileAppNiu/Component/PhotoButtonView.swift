9e6eb9d8b8df852d4a82dce0cba396a1632a26b2
aoxiangx <aoxiangx@student.unimelb.edu.au>
2024-10-04 15:51:37 +1000

    feat(photoview): modifed views to be dynamic and receive uploaded photos

--- Chi/MobileAppNiu/Component/PhotoButtonView.swift
+++ Chi/MobileAppNiu/Component/PhotoButtonView.swift
@@ -7,67 +7,49 @@
 
 import SwiftUI
 import PhotosUI
-import Photos
-
 
 struct PhotoButtonView: View {
-    @State private var selectedItems: [PhotosPickerItem] = [] // 用于存储选中的照片
-    @State private var selectedImages: [UIImage] = [] // 用于存储转换后的 UIImage
-    @State private var showPicker = false // 控制 PhotosPicker 的显示
+    @Binding var selectedImages: [UIImage] // 绑定外部传入的图片数组，更新图片
+    @State private var selectedItems: [PhotosPickerItem] = [] // 用于保存 PhotosPicker 选择的项目
 
     var body: some View {
-        VStack {
-            // 上传照片按钮
-            PhotosPicker(selection: $selectedItems, matching: .images) {
+        PhotosPicker(selection: $selectedItems, matching: .images) {
+            ZStack {
+                // 背景按钮样式
+                RoundedRectangle(cornerRadius: 8)
+                    .fill(Color.yellow)
+                    .frame(width: 48, height: 32)
+                
+                // 中间的加号图标
                 ZStack {
-                    // 黄色背景的按钮
-                    RoundedRectangle(cornerRadius: 8)
-                        .fill(Color.yellow)
-                        .frame(width: 48, height: 32)
+                    Rectangle()
+                        .fill(Color.gray)
+                        .frame(width: 16, height: 2)
                     
-                    // 灰色的加号图标
-                    ZStack {
-                        Rectangle()
-                            .fill(Color.gray)
-                            .frame(width: 16, height: 2)
-                        
-                        Rectangle()
-                            .fill(Color.gray)
-                            .frame(width: 2, height: 16)
-                    }
+                    Rectangle()
+                        .fill(Color.gray)
+                        .frame(width: 2, height: 16)
                 }
             }
-            .onChange(of: selectedItems) { newItems in
-                for newItem in newItems {
-                    Task {
-                        do {
-                            if let data = try await newItem.loadTransferable(type: Data.self),
-                               let image = UIImage(data: data) {
-                                // 如果选中的图片数量小于3张，则添加
-                                if selectedImages.count < 3 {
-                                    selectedImages.append(image)
-                                }
-                            }
-                        } catch {
-                            print("Error loading image: \(error)")
+        }
+        .onChange(of: selectedItems) { newItems in
+            for newItem in newItems {
+                Task {
+                    if let data = try? await newItem.loadTransferable(type: Data.self),
+                       let image = UIImage(data: data) {
+                        // 将图片添加到 selectedImages 数组中
+                        selectedImages.append(image)
+                        
+                        // 确保数组最多保存3张图片
+                        if selectedImages.count > 3 {
+                            selectedImages.removeFirst()
                         }
                     }
                 }
-            }
-
-            // 显示已选中的图片
-            ForEach(selectedImages, id: \.self) { image in
-                Image(uiImage: image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 200, height: 200) // 显示上传的照片
-                    .padding()
             }
         }
     }
 }
 
 
-#Preview {
-    PhotoButtonView()
-}
+
--- Chi/MobileAppNiu/Component/PhotoView.swift
+++ Chi/MobileAppNiu/Component/PhotoView.swift
@@ -8,55 +8,83 @@
 import SwiftUI
 
 struct PhotoView: View {
-    
-    // input 3 images
-    let image1: Image
-    let image2: Image
-    let image3: Image
+    var selectedImages: [UIImage] = [] // 保存用户上传的图片
     
     var body: some View {
-        
-        ZStack{
-        
-//            Image("BG")
-            image1
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: 320.6, height: 375.8)
-                .cornerRadius(16)
-                .rotationEffect(Angle(degrees: 5))
-                .shadow(radius: 2)
-            
-//            Image("BG")
-            image2
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: 320.6, height: 375.8)
-                .cornerRadius(16)
-                .rotationEffect(Angle(degrees: -5))
-                .offset(x: -5, y: 0)
-                .shadow(radius: 2)
-            
-            
-//            Image("BG")
-            image3
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: 320.6, height: 375.8)
-                .cornerRadius(16)
-                .rotationEffect(Angle(degrees: -15))
-                .offset(x: -10, y: 0)
-                .shadow(radius: 2)
-            
+        TabView {
+            if selectedImages.isEmpty {
+                // 没有图片时，显示默认的 BG 图片
+                ZStack {
+                    Image("BG")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 320.6, height: 375.8)
+                        .cornerRadius(16)
+                        .rotationEffect(Angle(degrees: 4))
+                        .shadow(radius: 2)
+
+                    Image("BG")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 320.6, height: 375.8)
+                        .cornerRadius(16)
+                        .rotationEffect(Angle(degrees: -2))
+                        .offset(x: -5, y: 0)
+                        .shadow(radius: 2)
+                    
+                    Image("BG")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 320.6, height: 375.8)
+                        .cornerRadius(16)
+                        .rotationEffect(Angle(degrees: -8))
+                        .offset(x: -10, y: 0)
+                        .shadow(radius: 2)
+                }
+            } else {
+                // 根据选择的图片展示，最多展示三张
+                ForEach(0..<min(selectedImages.count, 3), id: \.self) { index in
+                    ZStack {
+                        if index == 2 {
+                            Image(uiImage: selectedImages[index])
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 320.6, height: 375.8)
+                                .cornerRadius(16)
+                                .rotationEffect(Angle(degrees: -8))
+                                .offset(x: -10, y: 0)
+                                .shadow(radius: 2)
+                        }
+                        if index >= 1 {
+                            Image(uiImage: selectedImages[index - 1])
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 320.6, height: 375.8)
+                                .cornerRadius(16)
+                                .rotationEffect(Angle(degrees: -2))
+                                .offset(x: -5, y: 0)
+                                .shadow(radius: 2)
+                        }
+                        if index >= 0 {
+                            Image(uiImage: selectedImages[index])
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 320.6, height: 375.8)
+                                .cornerRadius(16)
+                                .rotationEffect(Angle(degrees: 4))
+                                .shadow(radius: 2)
+                        }
+                    }
+                }
+            }
         }
-        
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+        .frame(height: 500)
     }
 }
 
+
+
 #Preview {
-    PhotoView(
-        image1: Image("BG"),
-        image2: Image("BG"),
-        image3: Image("BG")
-    )
+    PhotoView()
 }
