f9e3e8f1daa2d272c0ac4f80623567d112de30c3
AHao23333 <hgguan@student.unimelb.edu.au>
2024-10-07 23:28:08 +1100

    feat(app.swift): add google sign in config at app file

--- Chi/MobileAppNiu/MobileAppNiuApp.swift
+++ Chi/MobileAppNiu/MobileAppNiuApp.swift
@@ -7,23 +7,27 @@
 
 import SwiftUI
 import FirebaseCore
+import GoogleSignIn
+import GoogleSignInSwift
 
 
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-
-    return true
-  }
-}
+//class AppDelegate: NSObject, UIApplicationDelegate {
+//  func application(_ application: UIApplication,
+//                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//    FirebaseApp.configure()
+//
+//    return true
+//  }
+//}
 
 
 @main
  struct MobileAppNiuApp: App {
   // register app delegate for Firebase setup
-     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+//     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+     
+     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+     
      @StateObject var manager = HealthManager()
      @StateObject var locationManager = LocationManager()
 
@@ -36,4 +40,21 @@ class AppDelegate: NSObject, UIApplicationDelegate {
       }
     }
   }
+}
+
+
+
+class AppDelegate: NSObject, UIApplicationDelegate{
+    func application(_ application: UIApplication,
+                        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey :
+                                                                        Any]? = nil) -> Bool{
+        FirebaseApp.configure()
+        return true
+    }
+    
+    @available(iOS 9.0, *)
+    func application(_ application: UIApplication, open url: URL, options:
+                     [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+        return GIDSignIn.sharedInstance.handle(url)
+    }
 }
