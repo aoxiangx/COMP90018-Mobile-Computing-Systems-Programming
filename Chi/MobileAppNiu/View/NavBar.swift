a5095507fb2a71dff0f1f1e86beaa425a4de4184
jun9a <jiuyuz@student.unimelb.edu.au>
2024-10-21 21:48:07 +1100

    Minor UI Fixed for Color in Dock and Changed the Grouping Data View into scroll view

---
+++ Chi/MobileAppNiu/View/NavBar.swift
@@ -0,0 +1,18 @@
+//
+//  NavBar.swift
+//  MobileAppNiu
+//
+//  Created by Jun Zhu on 21/10/2024.
+//
+
+import SwiftUI
+
+struct NavBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavBar()
+}
